7fa0ffcb3aa31f83c163579de5f529f45ebe9599
mukil <mukila.kanibalan@frenzo.io>
2023-08-22 16:52:26 +0530

    result calculation

--- chat_bot.py
+++ chat_bot.py
@@ -55,6 +55,11 @@ features = cols
 
 
 name = ""
+cnf_d = []
+sym = ''
+severarity = ''
+symptoms_exp = []
+symptoms_list = []
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -64,10 +69,6 @@ def get_name():
         name = str(request.form.get('name')).capitalize()
         return redirect(url_for('symptoms'))
     return render_template('form.html')
-
-
-cnf_d = []
-sym = ''
 
 
 @app.route('/symptoms', methods=['GET', 'POST'])
@@ -84,15 +85,53 @@ def symptoms():
                 return redirect('sevear')
             else:
                 return render_template('name.html', name=name, error="Enter valid symptom.")
-        return "we will"
     return render_template('name.html', name=name)
 
 
 @app.route('/sevear', methods=['GET', 'POST'])
 def sevear():
-    global cnf_d
-    print("therrrrrr", cnf_d)
+    global severarity, cnf_d
+    if request.method == 'POST':
+        try:
+            severarity = int(request.form.get('severarity'))
+            return redirect(url_for('symlist'))
+        except Exception as error:
+            print(error)
+            return render_template('sevear.html', name=name, symptom=sym, diesase=cnf_d, error="choose between numbers")
     return render_template('sevear.html', name=name, symptom=sym, diesase=cnf_d)
+
+
+@app.route('/symlist', methods=['GET', 'POST'])
+def symlist():
+    global cnf_d, name, sym, severarity, symptoms_exp, symptoms_list
+    if request.method == 'POST':
+        symptoms_exp = request.form.getlist('symptoms_list')
+        return redirect(url_for('number_day'))
+    symptoms_list = recurse(0, 1)
+    print("list are", symptoms_list)
+    return render_template('list.html', name=name, symptom=sym, diesase=cnf_d, severarity=severarity, symptoms_list=symptoms_list)
+
+
+sentence = precution_list = None
+number_of_days = None
+
+
+@app.route('/day', methods=['GET', 'POST'])
+def number_day():
+    global symptoms_exp, symptoms_list, sentence, precution_list, number_of_days
+    if request.method == 'POST':
+        day = int(request.form.get('day'))
+        number_of_days = day
+        sentence, precution_list = give_result(day)
+        return redirect(url_for('result'))
+
+    return render_template('day.html', name=name, symptom=sym, diesase=cnf_d, severarity=severarity, symptoms_list=symptoms_list, symptoms_exp=symptoms_exp)
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    global symptoms_exp, symptoms_list, sentence, precution_list, number_of_days
+    return render_template('result.html', name=name, symptom=sym, diesase=cnf_d, severarity=severarity, symptoms_list=symptoms_list, symptoms_exp=symptoms_exp, sentence=sentence, precution_list=precution_list, days=number_of_days)
 
 
 # ------------------------------------------------------------------------------
@@ -126,8 +165,11 @@ def calc_condition(exp, days):
         sum = sum+severityDictionary[item]
     if ((sum*days)/(len(exp)+1) > 13):
         print("You should take the consultation from doctor. ")
+        return "You should take the consultation from doctor. "
+
     else:
         print("It might not be that bad but you should take precautions.")
+        return "It might not be that bad but you should take precautions."
 
 
 def getDescription():
@@ -201,7 +243,15 @@ def print_disease(node):
     return list(map(lambda x: x.strip(), list(disease)))
 
 
-def tree_to_code(tree, feature_names, disease_input):
+feature_name = []
+disease_input = ''
+tree_ = None
+present_disease = None
+
+
+def tree_to_code(tree, feature_names, disease_in):
+    global feature_name, disease_input, tree_
+    disease_input = disease_in
     tree_ = tree.tree_
     feature_name = [
         feature_names[i] if i != _tree.TREE_UNDEFINED else "undefined!"
@@ -209,103 +259,56 @@ def tree_to_code(tree, feature_names, disease_input):
     ]
 
     chk_dis = ",".join(feature_names).split(",")
+    conf, cnf_dis = check_pattern(chk_dis, disease_in)
+    return conf, cnf_dis
+
+
+def recurse(node, depth):
+    global feature_name, disease_input, tree_, present_disease
     symptoms_present = []
+    indent = "  " * depth
+    if tree_.feature[node] != _tree.TREE_UNDEFINED:
+        name = feature_name[node]
+        threshold = tree_.threshold[node]
 
-    while True:
-
-        conf, cnf_dis = check_pattern(chk_dis, disease_input)
-        return conf, cnf_dis
-        # if conf == 1:
-        #     print("searches related to input: ")
-        #     for num, it in enumerate(cnf_dis):
-        #         print(num, ")", it)
-        #     if num != 0:
-        #         print(f"Select the one you meant (0 - {num}):  ", end="")
-        #         conf_inp = int(input(""))
-        #     else:
-        #         conf_inp = 0
-
-        #     disease_input = cnf_dis[conf_inp]
-        #     break
-        #     # print("Did you mean: ",cnf_dis,"?(yes/no) :",end="")
-        #     # conf_inp = input("")
-        #     # if(conf_inp=="yes"):
-        #     #     break
-        # else:
-        #     print("Enter valid symptom.")
-
-    while True:
-        try:
-            num_days = int(input("Okay. From how many days ? : "))
-            break
-        except:
-            print("Enter valid input.")
-
-    def recurse(node, depth):
-        indent = "  " * depth
-        if tree_.feature[node] != _tree.TREE_UNDEFINED:
-            name = feature_name[node]
-            threshold = tree_.threshold[node]
-
-            if name == disease_input:
-                val = 1
-            else:
-                val = 0
-            if val <= threshold:
-                recurse(tree_.children_left[node], depth + 1)
-            else:
-                symptoms_present.append(name)
-                recurse(tree_.children_right[node], depth + 1)
+        if name == disease_input:
+            val = 1
         else:
-            present_disease = print_disease(tree_.value[node])
-            # print( "You may have " +  present_disease )
-            red_cols = reduced_data.columns
-            symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero(
-            )]
-            # dis_list=list(symptoms_present)
-            # if len(dis_list)!=0:
-            #     print("symptoms present  " + str(list(symptoms_present)))
-            # print("symptoms given "  +  str(list(symptoms_given)) )
-            print("Are you experiencing any ")
-            symptoms_exp = []
-            for syms in list(symptoms_given):
-                inp = ""
-                print(syms, "? : ", end='')
-                while True:
-                    inp = input("")
-                    if (inp == "yes" or inp == "no"):
-                        break
-                    else:
-                        print("provide proper answers i.e. (yes/no) : ", end="")
-                if (inp == "yes"):
-                    symptoms_exp.append(syms)
+            val = 0
+        if val <= threshold:
+            val1 = recurse(tree_.children_left[node], depth + 1)
+            return list(val1)
+        else:
+            symptoms_present.append(name)
+            val2 = recurse(tree_.children_right[node], depth + 1)
+            return list(val2)
+    else:
+        present_disease = print_disease(tree_.value[node])
+        red_cols = reduced_data.columns
+        symptoms_given = red_cols[reduced_data.loc[present_disease].values[0].nonzero(
+        )]
+        return list(symptoms_given)
 
-            second_prediction = sec_predict(symptoms_exp)
-            # print(second_prediction)
-            calc_condition(symptoms_exp, num_days)
-            if (present_disease[0] == second_prediction[0]):
-                print("You may have ", present_disease[0])
-                print(description_list[present_disease[0]])
 
-                # readn(f"You may have {present_disease[0]}")
-                # readn(f"{description_list[present_disease[0]]}")
+def give_result(num_days):
+    global symptoms_exp, present_disease
+    second_prediction = sec_predict(symptoms_exp)
+    sentence = calc_condition(symptoms_exp, num_days)
+    if (present_disease[0] == second_prediction[0]):
+        print("You may have ", present_disease[0])
+        print(description_list[present_disease[0]])
 
-            else:
-                print("You may have ",
-                      present_disease[0], "or ", second_prediction[0])
-                print(description_list[present_disease[0]])
-                print(description_list[second_prediction[0]])
+    else:
+        print("You may have ",
+              present_disease[0], "or ", second_prediction[0])
+        print(description_list[present_disease[0]])
+        print(description_list[second_prediction[0]])
 
-            # print(description_list[present_disease[0]])
-            precution_list = precautionDictionary[present_disease[0]]
-            print("Take following measures : ")
-            for i, j in enumerate(precution_list):
-                print(i+1, ")", j)
-
-            # confidence_level = (1.0*len(symptoms_present))/len(symptoms_given)
-            # print("confidence level is " + str(confidence_level))
-
-    recurse(0, 1)
+    precution_list = precautionDictionary[present_disease[0]]
+    # print("Take following measures : ")
+    # for i, j in enumerate(precution_list):
+    #     print(i+1, ")", j)
+    return sentence, precution_list
 
 
 getSeverityDict()
@@ -314,11 +317,3 @@ getprecautionDict()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# getInfo()
-# while True:
-#     tree_to_code(clf, cols)
-#     next = input("wanna continue y/n :")
-#     print("----------------------------------------------------------------------------------------")
-#     if next.lower() == 'no':
-#         break
